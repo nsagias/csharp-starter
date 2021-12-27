45fab5a88026bf3cbfe6159e0b52b0ebbe456ad9
Nick <nsagias@gmail.com>
2021-12-27 16:50:57 -0500

    change console string

---
+++ 01_first_csharp_program/first_csharp_dotnet_project/first_csharp_dotnet_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first_csharp_dotnet_project
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is the first console string change");
+        }
+    }
+}
