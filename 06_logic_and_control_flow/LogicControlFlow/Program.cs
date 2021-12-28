00dbbbd33a2c941df013e94b3c2dc963173c3544
Nick <nsagias@gmail.com>
2021-12-27 21:38:22 -0500

    Create if, if else, else example

--- 06_logic_and_control_flow/LogicControlFlow/Program.cs
+++ 06_logic_and_control_flow/LogicControlFlow/Program.cs
@@ -3,10 +3,24 @@
 namespace LogicControlFlow
 {
     class Program
-    {
-        static void Main(string[] args)
+  {
+      static void Main(string[] args)
+      {
+        int num1 = 1;
+        int num2 = 2;
+        if (num1 < num2) 
         {
-            Console.WriteLine("Hello World!");
+          Console.WriteLine("num1 is less than num2");
+        } 
+        else if (num1 > num2) 
+        {
+          Console.WriteLine("num1 is less than num2");
         }
-    }
+        else 
+        {
+          Console.WriteLine("they might be eqaul");
+        }
+
+      }
+  }
 }
