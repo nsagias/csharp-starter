94a5981727ef7e876ed16844e606576bf2b8a05c
Nick <nsagias@gmail.com>
2021-12-27 21:56:29 -0500

    add comments and create string variable for ternary operator

--- 06_logic_and_control_flow/LogicControlFlow/Program.cs
+++ 06_logic_and_control_flow/LogicControlFlow/Program.cs
@@ -6,15 +6,21 @@ namespace LogicControlFlow
   {
       static void Main(string[] args)
       {
-
+        
+        // if statement variables
         int num1 = 1;
         int num2 = 2;
 
+        // switch statement variables
         string BINGO = "BINGO";
         string NOT_BINGO = "NOT_BINGO";
         string SOMEONE_ELSE_WON = "SOMEONE_ELSE_WON";
         string LULZ = "LULZ";
         string isBingo = LULZ;
+
+        // ternary operator variable
+
+        string result;
 
         // Logic Operators ( <, >, ==, >=, <=, != )
         if (num1 < num2) 
@@ -48,6 +54,8 @@ namespace LogicControlFlow
             Console.WriteLine(SOMEONE_ELSE_WON);
             break;
         }
+
+        
 
 
       }
