2fcf3d9810ab51c2b06527d257adc05a26c518eb
Nick <nsagias@gmail.com>
2021-12-27 22:08:23 -0500

    Create While and do while folder

---
+++ 07_while_and_do_while_loops/WhileAndDoWhile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhileAndDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
