b86cc5168de28ceeaca1032e49b3cae471455a7d
Nick <nsagias@gmail.com>
2021-12-27 22:21:30 -0500

    create decremeting while loop

--- 07_while_and_do_while_loops/WhileAndDoWhile/Program.cs
+++ 07_while_and_do_while_loops/WhileAndDoWhile/Program.cs
@@ -8,19 +8,42 @@ namespace WhileAndDoWhile
         {
             int num1 = 0;
             int num2 = 0;
+            int num3 = 5;
+            int num4 = 5;
             int numLoopLimit = 5;
+            int numLowerLimit = 0;
+
+            Console.WriteLine("------------ START -------------");
+            Console.WriteLine("--------------------------------");
+
 
             while (num1 <= numLoopLimit)
             {
               Console.WriteLine(num1);
               num1++;
             }
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("--------------------------------");
 
             do {
               Console.WriteLine(num2);
               num2++;
             }
             while (num2 < numLoopLimit);
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("--------------------------------");
+
+
+            while (num3 >= numLowerLimit)
+            {
+              Console.WriteLine(num3);
+              num3--;
+            }
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("--------------------------------");
+
+          
         }
     }
 }
