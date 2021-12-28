3041553f5311a1fc73d2ecb103d8547aadfb2eb1
Nick <nsagias@gmail.com>
2021-12-27 20:05:54 -0500

    Create readlines for each question

--- 05_types_and_inputs/TypesInputsOutputs/Program.cs
+++ 05_types_and_inputs/TypesInputsOutputs/Program.cs
@@ -19,10 +19,15 @@ namespace TypesInputsOutputs
           bool worksHard;
 
           Console.Write("Enter your name: ");
+          name = Console.ReadLine();
           Console.Write("Enter your age: ");
+          age = Console.ReadLine();
           Console.Write("Enter your: salary");
+          salary = Console.ReadLine();
           Console.Write("Are you fun?: ");
+          fun = Console.ReadLine();
           Console.Write("Do you work hard?: ");
+          worksHard = Console.ReadLine();
 
         }
     }
