351e8e8cdf61c1658bfbc5b91c5cb4b9eab2483b
Nick <nsagias@gmail.com>
2021-12-27 21:24:11 -0500

    Add try catch to catch user errors

--- 05_types_and_inputs/TypesInputsOutputs/Program.cs
+++ 05_types_and_inputs/TypesInputsOutputs/Program.cs
@@ -17,25 +17,29 @@ namespace TypesInputsOutputs
           double salary;
           bool fun;
           char worksHard;
+          
+          try {
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
 
-          Console.Write("Enter your name: ");
-          name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
 
-          Console.Write("Enter your age: ");
-          age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your salary: ");
+            salary = Convert.ToDouble(Console.ReadLine());
 
-          Console.Write("Enter your salary: ");
-          salary = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Are you fun? (true or false): ");
+            fun = Convert.ToBoolean(Console.ReadLine());
 
-          Console.Write("Are you fun? (true or false): ");
-          fun = Convert.ToBoolean(Console.ReadLine());
-
-          Console.Write("Do you work hard? (Y or N): ");
-          worksHard = Convert.ToChar(Console.ReadLine());
-
+            Console.Write("Do you work hard? (Y or N): ");
+            worksHard = Convert.ToChar(Console.ReadLine());
+      
           // Create console output
 
-          Console.WriteLine($"This is my console profile my name is {name} my age {age} I make this much {salary} it is true I am {fun}  do I work hard {worksHard}");
+           Console.WriteLine($"This is my console profile my name is {name} my age {age} I make this much {salary} it is true I am {fun}  do I work hard {worksHard}");
+          } catch  {
+            Console.WriteLine("Error: Something went wrong");
+          }
 
         }
     }
