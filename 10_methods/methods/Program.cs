dc4ce8b3dbc1db301ba2c90d5d55507fdf08af06
Nick <nsagias@gmail.com>
2021-12-28 21:43:31 -0500

    Create lamda that prints argument number provided

--- 10_methods/methods/Program.cs
+++ 10_methods/methods/Program.cs
@@ -6,10 +6,13 @@ namespace methods {
       Console.WriteLine("Standard PrintName");
     }
     static void LamdaPrintName() => Console.WriteLine("Lambda PrintName");
+    static void PrintNumber(int num) => Console.WriteLine($"{num}");
+
     
     static void Main(string[] args) {
       PrintName();
       LamdaPrintName();
+      PrintNumber(1);
     }
   }
 }
