41f7e4c07451d524bde3a61cec6cf32234966901
Nick <nsagias@gmail.com>
2021-12-29 17:29:56 -0500

    Create working async method

--- 11_task_promises/TaskPromises/Program.cs
+++ 11_task_promises/TaskPromises/Program.cs
@@ -11,7 +11,21 @@ namespace TaskPromises
        static void Main(string[] args)
         {
             Console.WriteLine("Task and Promisses poissibly async");
+            Console.WriteLine(DateTime.Now);
+            AsyncMethod();
+
         }
 
+         public static async Task AsyncMethod(){
+            await Task.Run(() => {
+              Console.WriteLine(DateTime.Now);
+              for (int i = 0; i < 100; i++){
+                Console.WriteLine($"Ansyc Method {DateTime.Now}");
+                Task.Delay(100).Wait();
+                
+              }
+              Console.WriteLine(DateTime.Now);
+          });
+        }
     }
 }
