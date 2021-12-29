427a56eeee58fac52208f481aa3f712557c3eb23
Nick <nsagias@gmail.com>
2021-12-28 21:57:24 -0500

    call another function with lamda function

--- 10_methods/methods/Program.cs
+++ 10_methods/methods/Program.cs
@@ -5,16 +5,20 @@ namespace methods {
     static void PrintName() {
       Console.WriteLine("Standard PrintName");
     }
-    static void LamdaPrintName() => Console.WriteLine("Lambda PrintName");
+    static void LamdaPrintName() => Console.WriteLine("Lamda PrintName");
     static void PrintNumber(int num) => Console.WriteLine($"{num}");
     static void PrintText(string text, string name) => Console.WriteLine($"{text} {name}");
+   
+    static int ReturnNumber1() =>  1;
 
+    static void LamdaReturnFunction() => Console.WriteLine(ReturnNumber1());
     
     static void Main(string[] args) {
       PrintName();
       LamdaPrintName();
       PrintNumber(1);
       PrintText("Hello from", "Nick");
+      LamdaReturnFunction();
 
     }
   }
