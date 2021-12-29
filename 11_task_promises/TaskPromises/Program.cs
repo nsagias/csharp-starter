3ce62276fa846bfeb7fbe4048d7f6bc1935b947e
Nick <nsagias@gmail.com>
2021-12-29 17:39:14 -0500

    Working async method

--- 11_task_promises/TaskPromises/Program.cs
+++ 11_task_promises/TaskPromises/Program.cs
@@ -8,24 +8,34 @@ namespace TaskPromises
 {
     class Program
     {
+
+    
        static void Main(string[] args)
         {
             Console.WriteLine("Task and Promisses poissibly async");
             Console.WriteLine(DateTime.Now);
             AsyncMethod();
+            AsyncMethod2();
+            Console.ReadKey();
 
         }
-
-         public static async Task AsyncMethod(){
+          public static async Task AsyncMethod(){
             await Task.Run(() => {
-              Console.WriteLine(DateTime.Now);
               for (int i = 0; i < 100; i++){
                 Console.WriteLine($"Ansyc Method {DateTime.Now}");
                 Task.Delay(100).Wait();
-                
               }
-              Console.WriteLine(DateTime.Now);
           });
         }
+          public static async Task AsyncMethod2(){
+            await Task.Run(() => {
+              for (int i = 0; i < 10; i++){
+                Console.WriteLine($"Ansyc Method 2 {DateTime.Now}");
+                Task.Delay(100).Wait();
+              }
+            });
+          }
+
+        
     }
 }
