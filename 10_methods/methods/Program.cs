022082946e4dd02936c6fb1a6dcb896d4e22e30f
Nick <nsagias@gmail.com>
2021-12-28 21:46:34 -0500

    create printext and provide to string elemnts

--- 10_methods/methods/Program.cs
+++ 10_methods/methods/Program.cs
@@ -7,12 +7,15 @@ namespace methods {
     }
     static void LamdaPrintName() => Console.WriteLine("Lambda PrintName");
     static void PrintNumber(int num) => Console.WriteLine($"{num}");
+    static void PrintText(string text, string name) => Console.WriteLine($"{text} {name}");
 
     
     static void Main(string[] args) {
       PrintName();
       LamdaPrintName();
       PrintNumber(1);
+      PrintText("Hello from", "Nick");
+
     }
   }
 }
