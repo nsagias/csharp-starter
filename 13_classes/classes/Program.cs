6928b87166b11f93101faf73870cc2217c201fab
Nick <nsagias@gmail.com>
2021-12-29 19:59:33 -0500

    Call person class and assign values inside try catch block

--- 13_classes/classes/Program.cs
+++ 13_classes/classes/Program.cs
@@ -4,24 +4,35 @@ namespace classes
 {
     class Program {
       static void Main(string[] args) {
-        int num = 0;
-
         Box box = new Box();
         box.Length = 10;
         box.Width = 15;
         box.Height = 20;
         double volume = box.getVolume();
-        
+
+        Person person = new Person();
         try {
-          Console.WriteLine($"{box.Length}, {box.Height}, {box.Width}");
-          Console.WriteLine($"Box volume {volume}");
-        } 
-        catch (Exception e) {
+          person.FirstName = "Nick";
+          person.LastName = "Sagias";
+          person.setAge(10);
+        } catch (Exception e) {
           Console.WriteLine(e.Message);
+          
+        } finally {
+          Console.WriteLine("Finally: create person function");
         }
-        finally {
-          Console.WriteLine("Functions ended");
-        }
+      
+        
+        // try {
+        //   Console.WriteLine($"{box.Length}, {box.Height}, {box.Width}");
+        //   Console.WriteLine($"Box volume {volume}");
+        // } 
+        // catch (Exception e) {
+        //   Console.WriteLine(e.Message);
+        // }
+        // finally {
+        //   Console.WriteLine("Functions ended");
+        // }
         
       }
     }
