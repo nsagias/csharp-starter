155d87be6c36965fb8c0025ecb593f596a9f5d0e
Nick <nsagias@gmail.com>
2021-12-29 19:00:11 -0500

    Create data type box

--- 13_classes/classes/Program.cs
+++ 13_classes/classes/Program.cs
@@ -4,9 +4,15 @@ namespace classes
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Classes");
+        static void Main(string[] args) {
+
         }
+    }
+
+    public class Box {
+      // this is also a data type
+      public double Length {get; set;}
+      public double Width { get; set; }
+      public double Height { get; set; }
     }
 }
