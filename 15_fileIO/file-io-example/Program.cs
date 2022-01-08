a1a016b5c67c5f11c22556f4eff5832a57f6e170
Nick <nsagias@gmail.com>
2022-01-08 18:16:48 -0500

    Refactor and clean up code, create variables for functions vs direct string names

--- 15_fileIO/file-io-example/Program.cs
+++ 15_fileIO/file-io-example/Program.cs
@@ -4,6 +4,20 @@ using System.Collections.Generic;
 
 
 
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var createNewDirectoryName = "salesTotalDirectory";
+var salesTotalDirectory = Path.Combine(currentDirectory, createNewDirectoryName  );
+
+Directory.CreateDirectory(salesTotalDirectory);
+
+var fileExtension = ".json";
+var salesFiles = FindFiles(storesDirectory, fileExtension);
+
+
+
+
 IEnumerable<string> FindFiles(string folderName, string fileExtension) {
   List<string> salesFiles = new List<string>();
 
@@ -20,29 +34,8 @@ IEnumerable<string> FindFiles(string folderName, string fileExtension) {
 
 
 
-
-// get current directory
-var currentDirectory = Directory.GetCurrentDirectory();
-
-// use current directory and add stores to path
-var storesDirectory = Path.Combine(currentDirectory, "stores");
-
-var fileExtension = ".json";
-
-var salesFiles = FindFiles(storesDirectory, fileExtension);
-
-
 foreach (var file in salesFiles) {
   Console.WriteLine(file);
 }
 
-
-// creating new directories
-Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores", "201", "newMadeDir"));
-
-// check if diretory exists  reuse current directory
-// var currentDirectory = Directory.GetCurrentDirectory();
-var filePath = Path.Combine(currentDirectory, "stores", "201","newMadeDir");
-bool directoryExists = Directory.Exists(filePath);
-Console.WriteLine($"Return boolean {directoryExists}");
 
