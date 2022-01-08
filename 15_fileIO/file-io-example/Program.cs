274744d6effcbd6dfe41c97d10ac71b22af84a57
Nick <nsagias@gmail.com>
2022-01-08 16:23:23 -0500

    Create function FindFiles, to loop through directory with files that end with sales.jon

--- 15_fileIO/file-io-example/Program.cs
+++ 15_fileIO/file-io-example/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System.IO;
 using System.Collections.Generic; 
 
-IEnumerable<string> FindFiles(string foldName) {
+IEnumerable<string> FindFiles(string folderName) {
   List<string> salesFiles = new List<string>();
+
+  var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
   
+  foreach (var file in foundFiles) {
+    if (file.EndsWith("sales.json")) {
+      salesFiles.Add(file);
+    }
+  }
+  return salesFiles;
 }
